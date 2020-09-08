d1804993e971d218ff543438ac6014c2ebb1eb2e
Rafael B <uounb@student.kit.edu>
2020-09-08 15:48:24 +0200

    Correct object extendion

--- scripts/worker.js
+++ scripts/worker.js
@@ -1,8 +1,12 @@
 importScripts("travizalign.js");
 onmessage = function (e) {
     console.log("start");
-    var traviz = e.data[0];
+    var traviz = new TRAViz("",{});
     var files = e.data[1];
+
+    for (const [key, value] of Object.entries(e.data[0])){
+        traviz[key] = value;
+    }
     traviz.prototype = TRAViz.prototype;
     traviz.align(files);
     postMessage([traviz, e.data[2]]);
