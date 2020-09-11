6546ddf50651fed8287ead4368921492b41ef677
Rafael B <45318499+raffiii@users.noreply.github.com>
2020-09-11 09:36:11 +0200

    Remove unnessecary prototype copy

--- scripts/worker.js
+++ scripts/worker.js
@@ -3,10 +3,6 @@ onmessage = function (e) {
     console.log("start");
     var traviz = new TRAViz("", new TRAVizConfig(e.data[2]));
     var files = e.data[1];
-
-    for (const [key, value] of Object.entries(e.data[0])) {
-        traviz[key] = value;
-    }
     traviz.align(files);
     postMessage([traviz, e.data[2]]);
 }
