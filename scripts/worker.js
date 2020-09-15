81e0005cb6591ca2fdf0ead3547423bfeb0ba027
Rafael B <uounb@student.kit.edu>
2020-09-15 10:58:36 +0200

    Correct merge

--- scripts/worker.js
+++ scripts/worker.js
@@ -3,18 +3,8 @@ onmessage = function (e) {
     console.log("start");
     let data = e.data[0];
     let traviz = new TRAViz("", data.config);
-    let files = data.files;
 
-    let entries = Object.getOwnPropertyNames(e.data[0]);
-    const index = entries.indexOf("config");
-    if (index > -1) {
-        entries.splice(index, 1);
-    }
-    for (const [key, value] of entries) {
-        traviz[key] = value;
-    }
-    traviz.prototype = TRAViz.prototype;
-    traviz.align(files);
+    traviz.align(data.files);
     var paths = this.aligner.getPathsByEdition(this.sentencePathHash[this.mainBranch], this.sentencePaths);
     postMessage([{
         "traviz": traviz,
